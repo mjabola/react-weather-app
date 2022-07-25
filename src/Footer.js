f572cf235bdab437f8bdbee0a922d03a6fe36f72
mjabola <99804271+mjabola@users.noreply.github.com>
2022-07-25 20:36:55 +1000

    Imported components from original project file

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Style.css";
+
+export default function Footer() {
+  return (
+    <div className="bottom-container">
+      Coded by Margarita Jabola
+      <br />
+      <a href="mailto:mhjabola@gmail.com">GET IN TOUCH</a>
+    </div>
+  );
+}
