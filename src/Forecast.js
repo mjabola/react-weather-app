130e706eb0778d54f1ab0ea5228ce877cc3ce30c
mjabola <99804271+mjabola@users.noreply.github.com>
2022-08-09 20:55:30 +1000

    Update Forecast.js

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,34 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import axios from "axios";
 
 import "./Forecast.css";
 
 export default function Forecast(props) {
-  function displayForecast(response)
-  {
-console.log(response.data)
+  const [ready, setReady] = useState(false);
+  const [forecastData, setForecastData] = useState(null);
+
+  function displayForecast(response) {
+    setForecastData(response.data.daily);
+    setReady(true);
+    console.log(response.data);
   }
 
-        let lat = props.coordinates.lat;
-  let lon = props.coordinates.lon;
+  if (ready) {
+    let maxTemp = Math.round(forecastData[0].temp.max);
+    let minTemp = Math.round(forecastData[0].temp.min);
+    let icon = forecastData[0].weather[0].icon;
+
+    return (
+      <div className="Forecast weather-forecast">
+        <div className="row">
+          <div className="col-2">
+            <WeatherIcon code={icon} size={25} />
+            <p className="day">Mon</p>
+            <p className="temp-forecast">
+              <span className="temp-forecast">{maxTemp}°</span>
+              <br />
+              <span className="min-temp-forecast">{minTemp}°</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let lat = props.coordinates.lat;
+    let lon = props.coordinates.lon;
     let apiKey = "45d865054195f39f1528ab2a051423dd";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayForecast);
 
-  return (
-    <div className="Forecast weather-forecast">
-      <div className="row">
-        <div className="col-2">
-          <WeatherIcon code="09d" size={25} />
-          <p className="day">Mon</p>
-          <p className="temp-forecast">
-            <span className="temp-forecast">21°</span>
-            <br />
-            <span className="min-temp-forecast">12°</span>
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+    return "Loading...";
+  }
 }
