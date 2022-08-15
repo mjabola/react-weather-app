90635a00051b6305e1357d20128930f279f802bd
mjabola <99804271+mjabola@users.noreply.github.com>
2022-08-16 09:38:42 +1000

    Styling icons

--- src/WeatherToday.js
+++ src/WeatherToday.js
@@ -6,38 +6,41 @@ import Temperature from "./Temperature";
 import "./Weather.css";
 
 export default function WeatherToday(props) {
-    return (
-      <div className="weather-info">
+  return (
+    <div className="weather-info">
+      <div className="row">
+        <div className="col-6">
+          <h3>{props.data.city}</h3>
+          <h2>
+            <Temperature celsius={props.data.temp} />
+          </h2>
+        </div>
+        <div className="col-6">
+          <WeatherIcon code={props.data.icon} size={60} />
+        </div>
+
         <div className="row">
           <div className="col-6">
-            <h3>{props.data.city}</h3>
-            <h2>
-                <Temperature celsius={props.data.temp} />
-            </h2>
+            <ul>
+              <li>{props.data.description}</li>
+              <li className="last-updated">
+                <FormatDate date={props.data.date} />
+              </li>
+            </ul>
           </div>
           <div className="col-6">
-              <WeatherIcon
-                code={props.data.icon} size={60}
-              />
-          </div>
-
-          <div className="row">
-            <div className="col-6">
-              <ul>
-                <li>{props.data.description}</li>
-                <li className="last-updated">
-                  <FormatDate date={props.data.date} />
-                </li>
-              </ul>
-            </div>
-            <div className="col-6">
-              <ul>
-                <li>Humidity: {props.data.humidity}%</li>
-                <li>Wind: {props.data.wind} km/h</li>
-              </ul>
-            </div>
+            <ul>
+              <li>
+                <i class="fa-solid fa-droplet"></i> Humidity:{" "}
+                {props.data.humidity}%
+              </li>
+              <li>
+                <i class="fa-solid fa-wind"></i> Wind: {props.data.wind} km/h
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
